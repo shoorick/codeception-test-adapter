2fd3a59f3a52d6e2384800a442d4740dd429a4fd
Alexander Sapozhnikov <shoorick@cpan.org>
2025-12-14 00:39:40 +0100

    Show dummy test, imitate its execution

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,58 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('Extension "codeception-test-adapter" is now active');
-
 	const controller = vscode.tests.createTestController(
 		'codeception',
 		'Codeception'
 	);
 
 	context.subscriptions.push(controller);
+
+	// Dummy suite
+	const suite = controller.createTestItem(
+		'dummy-suite',
+		'Dummy suite',
+		vscode.Uri.parse('file:///dummy')
+	);
+	controller.items.add(suite);
+
+	// Dummy test
+	const test = controller.createTestItem(
+		'dummy-test',
+		'it works',
+		vscode.Uri.parse('file:///dummy')
+	);
+	suite.children.add(test);
+
+	// Run handler
+	controller.createRunProfile(
+		'Run',
+		vscode.TestRunProfileKind.Run,
+		(request, token) => {
+			const run = controller.createTestRun(request);
+
+			const queue: vscode.TestItem[] = [];
+			if (request.include) {
+				queue.push(...request.include);
+			} else {
+				controller.items.forEach(item => queue.push(item));
+			}
+
+			for (const item of queue) {
+				run.started(item);
+
+				// imitate execution
+				setTimeout(() => {
+					run.passed(item);
+				}, 300);
+			}
+
+			setTimeout(() => run.end(), 400);
+		}
+	);
+
+	console.log('Codeception Test Adapter activated (dummy test)');
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
